f0cc1a51894c29ba23c046b7d3018f4621dc672c
Pablo Vazquez Diaz <AN0NlM0@hotmail.es>
2019-11-16 16:12:16 +0100

    PathFinding

---
+++ JamToday2019/Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject playerToFollow;
+    
+    void Update()
+    {
+        Vector3 Playerposition = new Vector3(playerToFollow.transform.position.x, playerToFollow.transform.position.y, -5);
+        transform.position = Playerposition;
+    }
+}
--- JamToday2019/Assets/Scripts/GameController.cs
+++ JamToday2019/Assets/Scripts/GameController.cs
@@ -6,18 +6,29 @@ public class GameController : MonoBehaviour
 {
     public float pastic;
     public int LevelNumber;
-
+    public List<LevelData> levels;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadLevel(0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadLevel(int LevelIndex)
+    {
+        List<GameObject> SpawnPoints = new List<GameObject>();
+        SpawnPoints.AddRange(GameObject.FindGameObjectsWithTag("SpawnPoint"));
+        LevelData Data = levels[LevelIndex];
+        foreach (Enemy enemy in Data.Enemies)
+        {
+            Instantiate(enemy.gameObject, SpawnPoints[Random.Range(0, SpawnPoints.Count)].transform);
+        }
     }
 }
